24fa45403bff1c3fa373908dabe37181dbd150ac
bedrantirak <bdrntrk@gmail.com>
2020-10-28 21:59:36 +0300

    Create data.js

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+
+   const data = ({
+    products: [
+        {
+        _id: "1",
+        name:"Slim Shirt",
+        category:"Shirts",
+        image:"/images/d1.jpg",
+        price: 60,
+        brand: " Nike",
+        rating: 4.5,
+        numReviews: 10,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing"
+      },
+      {
+        _id: "2",
+        name:"Fit Shirt",
+        category:"Shirts",
+        image:"/images/d1.jpg",
+        price: 50,
+        brand: " Nike",
+        rating: 4.1,
+        numReviews: 10,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing"
+      },
+      {
+        _id: "3",
+        name:"Best Pants",
+        category:"Pants",
+        image:"/images/d1.jpg",
+        price: 70,
+        brand: " Nike",
+        rating: 4.0,
+        numReviews: 8,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing"
+
+      }, 
+      {
+        _id: "4",
+        name:"Best Pants",
+        category:"Pants",
+        image:"/images/d1.jpg",
+        price: 70,
+        brand: " Nike",
+        rating: 4.0,
+        numReviews: 8,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing"
+
+      },
+
+]
+
+})
+export default data;
