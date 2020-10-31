bc2b88641ce35fdac15c77152286b231d4e7d57e
bedrantirak <bdrntrk@gmail.com>
2020-10-31 13:37:16 +0300

    Update CartScreen.js

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {addToCart} from "../actions/cartActions"
+
 function CartScreen(props){
- 
+
+   const cart = useSelector(state => state.cart);
+   const {cartItems} =cart;
    const productId = props.match.params.id;
    const qty = props.location.search? Number(props.location.search.split("=")[1]):1;
    const dispatch = useDispatch();
@@ -12,8 +15,44 @@ function CartScreen(props){
         }
     },[])
 
-    return <div>
-       Cart Screen
+    return <div className="cart">
+       <div className="cart-list">
+   <ul className="cart-list-container">
+       <li>
+           <h3>
+               Shopping Cart
+           </h3>
+           <div>
+               Price
+           </div>
+       </li>
+       {
+           cartItems.length === 0 ? <div>Cart is Empty</div> :
+           cartItems.map( item => <div><img src={item.image} alt="product" />
+           <div className="cart-name">
+               <div>
+                   {item.name}
+               </div>
+               <div>
+                   Qty:
+                  <select>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                  </select> 
+               </div>
+           </div>
+           <div>
+              $ {item.price}
+           </div>
+           </div>)
+       }
+   </ul>
+       </div>
+       <div className="cart-action">
+
+       </div>
     </div>
 }
 
