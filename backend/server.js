73bc62e635e0b5481d3fcda59efd665b314a2f34
bedrantirak <bdrntrk@gmail.com>
2020-11-02 17:09:29 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from "express";
-import data from "./data"
+import data from "./data";
+import config from "./config";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl,{
+   useNewUrlParser: true 
+}).catch(error=> console.log(error.reason));
+
+
+
 const app = express();
 
 app.get("/api/products/:id",(req, res)=>{
