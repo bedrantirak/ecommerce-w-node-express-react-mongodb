c90a920d26112b17c95a17bbbc85defac884bf47
bedrantirak <bdrntrk@gmail.com>
2020-11-02 16:34:36 +0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
 import {createStore,combineReducers, compose, applyMiddleware} from "redux";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 import thunk from "redux-thunk"
+import Cookie from "js-cookie"
 import {cartReducer} from "./reducers/cartReducers"
-const initialState ={};
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState ={ cart: {cartItems}};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
