ab1b9e8baf4f663f9f7ff7a68ea66a73c88da2ee
bedrantirak <bdrntrk@gmail.com>
2020-11-02 17:26:38 +0300

    Update userRoute.js

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import User from "../models/userModel"
+const router = express.Router();
+router.get("/api/users/createadmin",async (req,res)=>{
+   try {
+    const user = new User({
+        name:"Bedran",
+        email:"bdrntrk@gmail.com",
+        password: "123456789",
+        isAdmin: true
+
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+       
+   } catch (error) {
+       res.send({msg: error.message});
+   }
+  
+})
