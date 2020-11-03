243649b3cef4f304ebb55bad2ecb3ddaa3cbdec1
bedrantirak <bdrntrk@gmail.com>
2020-11-03 17:30:11 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import SigninScreen from './screens/SigninScreen';
 import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
 
 
 function App() {
@@ -57,7 +58,8 @@ function App() {
  </aside>
       <main className="main">
           <div className="content">
-            <Route path="/sigin" component={SigninScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?"  component={CartScreen} />
             <Route path="/" exact={true} component= {HomeScreen}/>
