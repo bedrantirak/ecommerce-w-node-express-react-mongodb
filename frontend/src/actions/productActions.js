a7571ff79d237d23e73a732c1e7d15a23d9e33f7
bedrantirak <bdrntrk@gmail.com>
2020-11-04 14:15:52 +0300

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,13 +1,16 @@
 
 import { PRODUCT_DETAILS_FAIL,
-     PRODUCT_DETAILS_REQUEST,
-      PRODUCT_DETAILS_SUCCESS,
-       PRODUCT_LIST_FAIL,
-        PRODUCT_LIST_REQUEST,
+         PRODUCT_DETAILS_REQUEST,
+         PRODUCT_DETAILS_SUCCESS,
+         PRODUCT_LIST_FAIL,
+         PRODUCT_LIST_REQUEST,
          PRODUCT_LIST_SUCCESS,
          PRODUCT_SAVE_SUCCESS,
          PRODUCT_SAVE_FAIL,
-         PRODUCT_SAVE_REQUEST
+         PRODUCT_SAVE_REQUEST,
+         PRODUCT_DELETE_REQUEST,
+         PRODUCT_DELETE_SUCCESS,
+         PRODUCT_DELETE_FAIL
          } from "../constants/productConstants";
 import axios from "axios"
 import Axios from "axios";
@@ -62,4 +65,22 @@ const detailsProduct = (productId) => async (dispatch) =>{
     }
 }
 
-export {listProducts, detailsProduct,saveProduct}
+
+const deleteProduct = (productId) => async (dispatch, getState) => {
+    try {
+      const {
+        userSignin: { userInfo },
+      } = getState();
+      dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
+      const { data } = await axios.delete('/api/products/' + productId, {
+        headers: {
+          Authorization: 'Bearer ' + userInfo.token,
+        },
+      });
+      dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data, success: true });
+    } catch (error) {
+      dispatch({ type: PRODUCT_DELETE_FAIL, payload: error.message });
+    }
+  };
+
+export {listProducts, detailsProduct,saveProduct,deleteProduct}
