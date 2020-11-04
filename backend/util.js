3521e5dc0a4d392ea9c115516953fcdcdbc71e80
bedrantirak <bdrntrk@gmail.com>
2020-11-04 14:46:43 +0300

    Update util.js

--- backend/util.js
+++ backend/util.js
@@ -21,12 +21,15 @@ const getToken = (user) => {
          if(err){
              return res.status(401).send({msg:"Invalid Token"});
          }
-        req.user = token;
+        req.user = decode;
         next();
         return
        } );
+   } else{
+    return res.status(401).send({msg:"Token is not supplied."})
+
    }
-   return res.status(401).send({msg:"Token is not supplied."})
+
     }
   
     const isAdmin = (req, res, next) => {
