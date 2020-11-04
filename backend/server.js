f4b6711fd2131f759350d29abe0b15f3a1687670
bedrantirak <bdrntrk@gmail.com>
2020-11-04 12:06:24 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
 import bodyParser from "body-parser";
+import productRoute from "./routes/productRoute";
 
 
 
@@ -23,18 +24,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
 
-app.get("/api/products/:id",(req, res)=>{
-    const productId = req.params.id;
-   const product = data.products.find(x => x._id === productId )
-   if(product)
-   res.send(product);
-   else 
-   res.status(404).send({msg:"Product Not Found."})
-});
 
-app.get("/api/products",(req, res)=>{
-    res.send(data.products);
-});
+// app.get("/api/products/:id",(req, res)=>{
+//     const productId = req.params.id;
+//    const product = data.products.find(x => x._id === productId )
+//    if(product)
+//    res.send(product);
+//    else 
+//    res.status(404).send({msg:"Product Not Found."})
+// });
+
+// app.get("/api/products",(req, res)=>{
+//     res.send(data.products);
+// });
 
 app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
