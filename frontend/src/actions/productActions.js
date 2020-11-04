804c6d57f3b7a7c3b02e181024eed92adbf417ba
bedrantirak <bdrntrk@gmail.com>
2020-11-04 12:06:28 +0300

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,9 +4,13 @@ import { PRODUCT_DETAILS_FAIL,
       PRODUCT_DETAILS_SUCCESS,
        PRODUCT_LIST_FAIL,
         PRODUCT_LIST_REQUEST,
-         PRODUCT_LIST_SUCCESS
+         PRODUCT_LIST_SUCCESS,
+         PRODUCT_SAVE_SUCCESS,
+         PRODUCT_SAVE_FAIL,
+         PRODUCT_SAVE_REQUEST
          } from "../constants/productConstants";
 import axios from "axios"
+import Axios from "axios";
 
 const listProducts = () => async (dispatch) => {
     try{
@@ -24,6 +28,21 @@ const listProducts = () => async (dispatch) => {
    
    
 }
+
+const saveProduct = (product) => async (dispatch,getState) =>{
+    try {
+        dispatch({type: PRODUCT_SAVE_REQUEST, payload: product});
+        const {userSignin:{userInfo}} = getState();
+        const {data} = await Axios.post("/api/products",product,{headers:{
+            "Authorization":"Bearer " + userInfo.token 
+        }});
+        dispatch({type:PRODUCT_SAVE_SUCCESS, payload: data});
+
+    } catch (error) {
+                dispatch({type:PRODUCT_SAVE_FAIL, payload: error.message});
+
+    }
+}
 const detailsProduct = (productId) => async (dispatch) =>{
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
@@ -34,4 +53,4 @@ const detailsProduct = (productId) => async (dispatch) =>{
     }
 }
 
-export {listProducts, detailsProduct}
+export {listProducts, detailsProduct,saveProduct}
