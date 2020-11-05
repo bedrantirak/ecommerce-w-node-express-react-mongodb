eead08d6cbd239b62d2147ecaa38437304f58bbe
bedrantirak <bdrntrk@gmail.com>
2020-11-05 10:40:10 +0300

    Update cartReducers.js

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,7 @@
 
 import { CART_ADD_ITEM,CART_REMOVE_ITEM, CART_SAVE_PAYMENT, CART_SAVE_SHIPPING } from "../constants/cartConstants";
 
-function cartReducer(state={cartItems:[]}, action){
+function cartReducer(state={cartItems:[],shipping:{},payment:{} },action){
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload;
