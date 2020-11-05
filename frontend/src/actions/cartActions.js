fd7c31c0afa863d5692f913edffd0bb60b7a2c6b
bedrantirak <bdrntrk@gmail.com>
2020-11-05 08:17:41 +0300

    Update cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Cookie from "js-cookie";
-import {CART_ADD_ITEM,CART_REMOVE_ITEM,CART_SAVE_SHIPPING} from "../constants/cartConstants";
+import {CART_ADD_ITEM,CART_REMOVE_ITEM,CART_SAVE_PAYMENT,CART_SAVE_SHIPPING} from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch,getState) =>{
 
@@ -31,4 +31,7 @@ const removeFromCart = (productId) => (dispatch,getState) =>{
 const saveShipping = (data) => (dispatch) => {
     dispatch({ type: CART_SAVE_SHIPPING, payload: data });
   }
-export {addToCart,removeFromCart,saveShipping} 
+  const savePayment = (data) => (dispatch) => {
+    dispatch({ type: CART_SAVE_PAYMENT, payload: data });
+  }
+export {addToCart,removeFromCart,saveShipping,savePayment} 
